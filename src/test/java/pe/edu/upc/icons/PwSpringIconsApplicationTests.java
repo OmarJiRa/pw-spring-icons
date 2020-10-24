f2e21a5b3bdefa0a95b6d4e3ae530146a5e4216e
OmarJiRa <jimenezramirezomar02@gmail.com>
2020-10-24 13:36:01 -0500

    Configured and tested

--- src/test/java/pe/edu/upc/icons/PwSpringIconsApplicationTests.java
+++ src/test/java/pe/edu/upc/icons/PwSpringIconsApplicationTests.java
@@ -1,13 +1,30 @@
 package pe.edu.upc.icons;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import pe.edu.upc.icons.models.entities.Categoria;
+import pe.edu.upc.icons.services.CategoriaService;
 
 @SpringBootTest
 class PwSpringIconsApplicationTests {
+	
+	@Autowired
+	private CategoriaService categoriaService;
 
 	@Test
 	void contextLoads() {
+		Categoria cat = new Categoria();
+		cat.setNombre("Viajes");
+		cat.setDescripcion("Sección de viajes porque somos bien viajeros");
+		
+		try {
+			categoriaService.save(cat);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
