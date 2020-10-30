80afe9d6e4d51b258f5a12ea7a4a132ab8cfe5a2
fabricio-davila <72274534+fabricio-davila@users.noreply.github.com>
2020-10-29 21:24:32 -0500

    Added US18_FabricioDavila

---
+++ src/main/java/pe/edu/upc/icons/services/ComunidadPostService.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.icons.services;
+
+import java.util.List;
+
+import pe.edu.upc.icons.models.entities.ComunidadPost;
+
+public interface ComunidadPostService extends CrudService<ComunidadPost, Integer> {
+	List<ComunidadPost> findByNombre(String nombre) throws Exception;
+}
