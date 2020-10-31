9cbf6ab798f79d7f634a4b448e17279770da4e27
AshleyJLlo <ashley.612@hotmail.com>
2020-10-31 14:45:01 -0500

    Actualizacion Post

--- src/main/java/pe/edu/upc/icons/models/repositories/ComentariosRepository.java
+++ src/main/java/pe/edu/upc/icons/models/repositories/ComentariosRepository.java
@@ -9,11 +9,5 @@ import pe.edu.upc.icons.models.entities.Comentarios;
 
 @Repository
 public interface ComentariosRepository extends JpaRepository<Comentarios, Integer>{
-<<<<<<< HEAD
-	
-	//List<Comentarios> findByNombreLike(String nombre) throws Exception;
 
-=======
-	/*List<Comentarios> findByNombre(String nombre) throws Exception;*/
->>>>>>> 2d45f0f4288a82084695a3ea1835c2c83ac57b07
 }
