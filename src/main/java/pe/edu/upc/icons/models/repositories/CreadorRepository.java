55d240be216326037bfb86caa06bb5fc5e15e3f8
AshleyJLlo <ashley.612@hotmail.com>
2020-11-14 15:27:00 -0500

    Added US12 v2

--- src/main/java/pe/edu/upc/icons/models/repositories/CreadorRepository.java
+++ src/main/java/pe/edu/upc/icons/models/repositories/CreadorRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.icons.models.entities.Creador;
@@ -17,17 +18,17 @@ public interface CreadorRepository extends JpaRepository<Creador, Integer> {
 	
 	@Query("SELECT COUNT(u.usuario) " + 
 			"FROM UsuarioPost u JOIN Post p ON u.post = p.id JOIN Creador c ON c.id = p.creador " + 
-			"WHERE c.id = 1 AND u.meGusta = true")
-	Integer numeroMeGusta(Integer id) throws Exception;
+			"WHERE c.id = :id AND u.meGusta = true")
+	Integer numeroMeGusta(@Param("id") Integer id) throws Exception;
 	
 	@Query("SELECT COUNT(u.usuario) "
 			  + "FROM UsuarioPost u JOIN Post p ON u.post = p.id JOIN Creador c ON c.id = p.creador "
-			  + "WHERE u.comentario != ''")
-	Integer numeroComentarios(Integer id) throws Exception;
+			  + "WHERE u.comentario != '' AND c.id= :id")
+	Integer numeroComentarios(@Param("id")Integer id) throws Exception;
 	
-	@Query("SELECT COUNT(s.usuario) FROM Seguimiento s WHERE s.creador = 1")
-	Integer numeroSeguidores(Integer id) throws Exception;
+	@Query("SELECT COUNT(s.usuario) FROM Seguimiento s WHERE s.creador.id = :id")
+	Integer numeroSeguidores(@Param("id")Integer id) throws Exception;
 	
-	@Query("SELECT COUNT(s.usuario) FROM Suscripcion s WHERE s.creador = 1")
-	Integer numeroSuscriptores(Integer id) throws Exception;
+	@Query("SELECT COUNT(s.usuario) FROM Suscripcion s WHERE s.creador.id = :id")
+	Integer numeroSuscriptores(@Param("id")Integer id) throws Exception;
 }
